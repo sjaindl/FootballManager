aa9ae2332b1ff8a183b45e59c69936fa7ee2a8df
Stefan Jaindl <jaindl.stefan@gmail.com>
2019-02-17 22:18:09 +0100

    feat: AboutUsComponent

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Title, Meta } from '@angular/platform-browser'
+import { FirebaseService } from '../services/firebase.service'
+import { Chef } from '../shared/chefs'
 
 @Component({
   selector: 'app-aboutus',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutusComponent implements OnInit {
 
-  constructor() { }
+  chefs: Chef[] = []
+  constructor(public firebaseService: FirebaseService, 
+    private titleService: Title, private metaTagService: Meta) { }
 
   ngOnInit() {
-  }
+    this.titleService.setTitle("Fußball Manager: Über uns")
+    this.metaTagService.updateTag({
+      name: 'description', content: "Informationen rund über den Fußball Manager"
+    })
 
+    this.firebaseService.getChefs().valueChanges().subscribe((chefArray) => {
+      chefArray.forEach(element => {
+        let chef = new Chef()
+        chef.init(element)
+        this.chefs.push(chef)
+      })
+    })
+  }
 }
