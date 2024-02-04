a75ba5cdbbd365cb6ef806f9934c90dc40469b86
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-02-04 11:32:57 +0100

    Add base route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [{ path: 'home', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+];
