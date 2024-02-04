621966658385b8772747d18745a45d77b80533cf
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-02-04 18:35:51 +0100

    feat: Cookie consent

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,34 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import {
+  NgcCookieConsentConfig,
+  provideNgcCookieConsent,
+} from 'ngx-cookieconsent';
+import { routes } from './app.routes';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  cookie: {
+    domain: 'starting-eleven-2019.firebaseapp.com',
+  },
+  palette: {
+    popup: {
+      background: '#c2185b',
+    },
+    button: {
+      background: '#f1d600',
+    },
+  },
+  theme: 'edgeless',
+  type: 'opt-out',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideAnimationsAsync(),
+    provideNgcCookieConsent(cookieConfig),
   ],
 };
