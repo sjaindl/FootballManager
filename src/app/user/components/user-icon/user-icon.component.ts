8f62d74c7cd114d0e408c05398996b6d0519be52
Georg Schober <g.schober2011@gmail.com>
2024-02-11 19:08:06 +0100

    feat: profile icon

--- src/app/user/components/user-icon/user-icon.component.ts
+++ src/app/user/components/user-icon/user-icon.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatFabButton } from '@angular/material/button';
+// import { Mat_Ico } from '@angular/material/icon';
 
 @Component({
   selector: 's11-user-icon',
   standalone: true,
-  imports: [],
+  imports: [MatFabButton],
   templateUrl: './user-icon.component.html',
   styleUrl: './user-icon.component.scss',
 })
