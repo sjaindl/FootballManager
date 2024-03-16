df78130b31567863cdcd3e8751dc21fe64d24360
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-03-16 19:36:19 +0100

    feat(auth): Add AuthStore & user persistance

---
+++ src/app/auth/store/auth.store.ts
@@ -0,0 +1,49 @@
+import { computed } from '@angular/core';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { produce } from 'immer';
+import { User } from '../../shared/user';
+
+interface AuthState {
+  user?: User;
+}
+
+const initialState: AuthState = {
+  user: undefined,
+};
+
+export const AuthStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+
+  withComputed(({ user }) => ({
+    uid: computed(() => {
+      if (user) {
+        return user()?.uid ?? '';
+      }
+      return '';
+    }),
+    userName: computed(() => {
+      if (user) {
+        return user()?.userName ?? undefined;
+      }
+      return undefined;
+    }),
+  })),
+
+  withMethods(store => ({
+    setUser(user?: User): void {
+      patchState(store, state => {
+        // state.uid = uid
+        return produce(state, draft => {
+          draft.user = user;
+        });
+      });
+    },
+  }))
+);
