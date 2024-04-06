b8df9d18b1c36ab5b9088a27f50a9f6572eceec7
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-04-06 12:43:03 +0200

    feat(player): Flex UI & Points logic

--- src/app/shared/player/player.component.ts
+++ src/app/shared/player/player.component.ts
@@ -2,17 +2,16 @@ import { CommonModule } from '@angular/common';
 import {
   Component,
   EventEmitter,
-  OnChanges,
   OnInit,
   Output,
-  SimpleChanges,
-  WritableSignal,
+  Signal,
+  computed,
   input,
-  signal,
 } from '@angular/core';
 import { Storage, getDownloadURL, ref } from '@angular/fire/storage';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { ChangePlayerRequest, Player } from '../common.model';
 
@@ -25,21 +24,47 @@ import { ChangePlayerRequest, Player } from '../common.model';
     MatSelectModule,
     FormsModule,
     ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
   ],
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss',
 })
-export class PlayerComponent implements OnChanges, OnInit {
+export class PlayerComponent implements OnInit {
   player = input<Partial<Player>>();
   playerList = input<Player[]>([]);
+  displayPointsSeparately = input(true);
+  isSelectable = input(true);
+  isAdmin = input(false);
+  editMode = input<boolean>(false);
+  matchDayId = input<string>();
   @Output() selectedPlayerChange = new EventEmitter<ChangePlayerRequest>();
-
-  player$: WritableSignal<Partial<Player>> = signal({});
-  playerList$: WritableSignal<Player[]> = signal([]);
 
   imageUrl: Promise<String> | undefined;
 
-  constructor(private storage: Storage) {}
+  points: Signal<number>;
+  pointsCurrentRound: Signal<number>;
+
+  constructor(private storage: Storage) {
+    this.points = computed(() => {
+      const matchDayId = this.matchDayId();
+      const points = this.player()?.points;
+      if (!points) {
+        return 0;
+      }
+
+      if (matchDayId) {
+        if (points.hasOwnProperty(matchDayId)) {
+          return points[matchDayId] ?? 0;
+        }
+        return 0;
+      }
+
+      return this.totalPoints(points);
+    });
+
+    this.pointsCurrentRound = computed(() => 0);
+  }
 
   ngOnInit(): void {
     const playerRef = this.player()?.imageRef;
@@ -53,20 +78,25 @@ export class PlayerComponent implements OnChanges, OnInit {
     }
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    const player = this.player();
-    if (changes['player'] && player) {
-      this.player$.set(player);
-    }
-    if (changes['playerList'] || changes['player']) {
-      this.playerList$.set(this.playerList());
-    }
-  }
-
   onValueChange(playerId: string) {
     this.selectedPlayerChange.emit({
       newPlayerId: playerId,
       oldPlayerId: this.player()?.playerId ?? '',
     });
+  }
+
+  totalPoints(pointsOptional: Record<string, number> | undefined): number {
+    const points = pointsOptional;
+    if (!points) {
+      return 0;
+    }
+
+    var totalPoints = 0;
+
+    Object.entries(points).forEach(([matchDayKey, pointsOfMatchDay]) => {
+      totalPoints += pointsOfMatchDay;
+    });
+
+    return totalPoints;
   }
 }
