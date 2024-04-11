e0148eb69221ab71bdf2e2508fa606c4bb5825be
Stefan Jaindl <jaindl.stefan@gmail.com>
2024-04-12 00:58:04 +0200

    feat(players): Show all players for total category

--- src/app/admin/components/matchday/matchday.component.ts
+++ src/app/admin/components/matchday/matchday.component.ts
@@ -18,11 +18,13 @@ import { MatchdayStore } from '../../store/matchday.store';
 export class MatchdayComponent {
   matchday = input<string>();
   editMode = input<boolean>(false);
+  singleRow = input<boolean>(false);
+  isAdmin = input<boolean>(true);
 
   readonly playerStore = inject(PlayerStore);
   readonly matchdayStore = inject(MatchdayStore);
 
-  players: Signal<Player[]>;
+  players: Player[];
 
   goalkeeper: Signal<Player[]>;
   defenders: Signal<Player[]>;
@@ -30,7 +32,14 @@ export class MatchdayComponent {
   attacker: Signal<Player[]>;
 
   constructor() {
-    this.players = this.playerStore.players;
+    const totalPoints = this.playerStore.totalPoints();
+    this.players = this.playerStore.players().sort((first, second) => {
+      const firstPoints = totalPoints[first.playerId];
+      const secondPoints = totalPoints[second.playerId];
+      if (firstPoints > secondPoints) return -1;
+      if (firstPoints < secondPoints) return 1;
+      else return 0;
+    });
 
     this.goalkeeper = this.playerStore.goalkeepers;
     this.defenders = this.playerStore.defenders;
